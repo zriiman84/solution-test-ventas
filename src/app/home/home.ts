784c6f0a2eb7c353819cfde5ff126c3c7daf2c15
Cristofer_Vargas <crisric84@gmail.com>
2025-11-18 22:29:38 -0500

    Maquetacion y diseño del componente home

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,19 +3,12 @@ import { HomeHeader } from './home-header/home-header';
 import { Footer } from '../shared/components/footer/footer';
 import { ProductCard } from '../shared/components/product-card/product-card';
 import { Producto } from '../shared/models/product.model';
-<<<<<<< HEAD
-
-@Component({
-  selector: 'app-home',
-  imports: [HomeHeader,ProductCard,Footer],
-=======
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
   imports: [HomeHeader,ProductCard,Footer,MatFormFieldModule,MatSelectModule],
->>>>>>> e65150a (Maquetación e implementación del componente home-header, footer y product-card)
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -23,7 +16,6 @@ export class Home {
 
   obtenerProducto(valorBusqueda: string) {
     console.log('Producto buscado en Home:', valorBusqueda);
-
   }
 
    productoTest: Producto = {
