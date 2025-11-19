5f26ad7bb3f2d20aa7a9c43683cbe0835782c552
Cristofer_Vargas <crisric84@gmail.com>
2025-11-18 20:26:58 -0500

    Maquetación e implementación del componente home-header, footer y product-card

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,10 +3,19 @@ import { HomeHeader } from './home-header/home-header';
 import { Footer } from '../shared/components/footer/footer';
 import { ProductCard } from '../shared/components/product-card/product-card';
 import { Producto } from '../shared/models/product.model';
+<<<<<<< HEAD
 
 @Component({
   selector: 'app-home',
   imports: [HomeHeader,ProductCard,Footer],
+=======
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-home',
+  imports: [HomeHeader,ProductCard,Footer,MatFormFieldModule,MatSelectModule],
+>>>>>>> e65150a (Maquetación e implementación del componente home-header, footer y product-card)
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
