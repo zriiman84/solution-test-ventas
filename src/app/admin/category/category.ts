8a2e368916aefc7a754c92289a4c17dc3d02d5db
Cristofer_Vargas <crisric84@gmail.com>
2025-12-08 12:47:14 -0500

    Reubicación del componente product dentro de admin. Creación de nuevos componentes para el menú de admin.

---
+++ src/app/admin/category/category.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  imports: [],
+  templateUrl: './category.html',
+  styleUrl: './category.css',
+})
+export class Category {
+
+}
---
+++ src/app/admin/sales/sales.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales',
+  imports: [],
+  templateUrl: './sales.html',
+  styleUrl: './sales.css',
+})
+export class Sales {
+
+}
